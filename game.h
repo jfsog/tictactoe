923fb722e376b3d99e7a6d3738b790bef7d8e065
jfsog <felipe.sg@protonmail.com>
2023-07-13 11:04:57 -0300

    descricao em game.h

--- game.h
+++ game.h
@@ -1,9 +1,16 @@
 #ifndef GAME_H_
 #define GAME_H_
 #include <stdbool.h>
-
-#define DEFGAME 3
+/*
+ * N define as dimensões do tabuleiro do jogo
+ * Fique a vontade para alterar esse valor, mas o algoritimo não lida muito bem
+ * com valores muito acima de 3.
+ * O limite máximo para N é 11, pois o algoritimo converte o tabuleiro em uma
+ * representação utilizando um inteiro de 128 bits.
+ * logo 11 é o maior valor para N^2 <= 128
+ * */
 #define N 3
+#define DEFGAME 3
 #define SCREEN_WIDTH 640.0
 #define SCREEN_HEIGHT 480.0
 #define CELL_WIDTH (SCREEN_WIDTH / N)
@@ -22,6 +29,8 @@
 #define HWI_POINTS 100000
 #define WIN_POINTS 1000000
 
+// total de partidas que podem ser jogadas
+// jogador e cpu jogam de forma alternada
 #define TOTALPARTIDAS 10
 typedef struct {
   int board[N * N];
